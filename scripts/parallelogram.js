b527c31193134f530f68d1728deb5d9969faf936
azmaeenmahtab <mahtabazmaeen7@gmail.com>
2024-11-09 23:10:35 +0600

    checking

--- scripts/parallelogram.js
+++ scripts/parallelogram.js
@@ -9,3 +9,13 @@ function areaParallelogram() {
     let area = .5 * para_b * para_h;
     displayArea('display-area-parallelogram', area)
 }
+
+function getInput(elementId) {
+    let inputedValue = document.getElementById(elementId).value;
+    let input = parseFloat(inputedValue)
+    return input;
+}
+
+function displayArea(elementId, area) {
+    document.getElementById(elementId).innerText = 'Area of parallelogram is = ' + area + ' cm²';
+}
