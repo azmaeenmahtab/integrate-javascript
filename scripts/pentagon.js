6cde0d2f1d60045655a18651beef1772b3e75bc7
azmaeenmahtab <mahtabazmaeen7@gmail.com>
2024-11-09 22:45:13 +0600

    checking

---
+++ scripts/pentagon.js
@@ -0,0 +1,22 @@
+function areaPentagon() {
+
+    let pent_p = getInput('input-pentagon-p')
+
+
+    let pent_b = getInput('input-pentagon-b')
+
+
+    let area = .5 * pent_b * pent_p;
+    displayArea('display-area-pentagon', area)
+}
+
+
+function getInput(elementId) {
+    let inputedValue = document.getElementById(elementId).value;
+    let input = parseFloat(inputedValue)
+    return input;
+}
+
+function displayArea(elementId, area) {
+    document.getElementById(elementId).innerText = 'Area is = ' + area + ' cm²';
+}
